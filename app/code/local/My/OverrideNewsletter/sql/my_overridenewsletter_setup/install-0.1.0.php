952709546074bf25cd09192cf080612b145b4af4
frankovv <frankovv@mail.ru>
2016-06-15 19:44:19 +0300

    override controller

--- app/code/local/My/OverrideNewsletter/sql/my_overridenewsletter_setup/install-0.1.0.php
+++ app/code/local/My/OverrideNewsletter/sql/my_overridenewsletter_setup/install-0.1.0.php
@@ -9,8 +9,8 @@ $installer->getConnection()
     ->addColumn($installer->getTable('newsletter_subscriber'),
         'subscriber_name',
         array(
-            'type' => Varien_Db_Ddl_Table::TYPE_TIMESTAMP,
-            'nullable' => true,
+            'type' => Varien_Db_Ddl_Table::TYPE_VARCHAR,
+            'length' => 50,
             'default' => null,
             'comment' => 'Subscriber Name'
         )
