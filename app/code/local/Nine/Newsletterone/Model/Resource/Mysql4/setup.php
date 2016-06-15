5345c3bac01b9b5aa1caba68568f187cf352a488
frankovv <frankovv@mail.ru>
2016-06-15 16:00:43 +0300

    +

--- app/code/local/Nine/Newsletterone/Model/Resource/Mysql4/setup.php
+++ app/code/local/Nine/Newsletterone/Model/Resource/Mysql4/setup.php
@@ -5,7 +5,7 @@
  * Date: 15.06.2016
  * Time: 15:12
  */
-class My_Newsletteron_Model_Resource_Mysql4_Setup extends Mage_Core_Model_Resource_Setup
+class Nine_Newsletterone_Model_Resource_Mysql4_Setup extends Mage_Core_Model_Resource_Setup
 {
 
 }
